747235f89aaa481f1063bda282c709799c71d610
Hanseul Shim <hanseulshim@gmail.com>
2017-02-22 09:57:33 -0600

    day2

---
+++ HackerRank/30DaysOfCodeChallenges/Day5-Loops.js
@@ -0,0 +1,12 @@
+/*
+Task 
+Given an integer, n, print its first 10 multiples. Each multiple n x i (where 1 <= i <= 10) should be printed on a new line in the form: n x i = result.
+*/
+
+
+function main() {
+    var n = parseInt(readLine());
+    for(var i = 1; i<=10; i++){
+        console.log(n + ' x ' + i + ' = ' + (n*i))
+    }
+}

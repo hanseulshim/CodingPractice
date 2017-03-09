79ae9c2bce6b195dd6e14f2cc2447599713a12a8
Hanseul Shim <hanseulshim@gmail.com>
2017-03-09 15:37:34 -0600

    linkedList

---
+++ HackerRank/DataStructures/LinkedList/14-InsertANode.java
@@ -0,0 +1,18 @@
+Node SortedInsert(Node head,int data) {
+    Node n = new Node();
+    n.data = data;
+    if (head == null) {
+        return n;
+    }
+    else if (data <= head.data) {
+        n.next = head;
+        head.prev = n;
+        return n;
+    }
+    else {
+        Node rest = SortedInsert(head.next, data);
+        head.next = rest;
+        rest.prev = head;
+        return head;
+    }
+}
